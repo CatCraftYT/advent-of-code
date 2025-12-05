77c8d85aac0d82520125e0f92cdf8a64d38d4e6e
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2025-12-06 00:48:11 +1030

    Added day 3 part 2 solution

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This branch contains all of my solutions to [Advent of Code 2025](https://adventofcode.com). All solutions are written in Haskell. Using Cabal this time (as annoying as it can be)!
 
 # Unsolved
-- Day 3 (part 2)
+None *so far*...
--- solutions/day3-2.hs
+++ solutions/day3-2.hs
@@ -1,14 +1,28 @@
 module Main where
-import Data.List
 
-combinations :: String -> [String]
-combinations = filter ((==12) . length) . subsequences
+-- All combinations of `l` with 1 element removed.
+removals :: [a] -> [[a]]
+removals l = [removal n | n <- [0.. length l - 1]]
+  where removal n = pre ++ tail post
+          where (pre, post) = splitAt n l
+
+-- Just remove one digit, pick the one that results in the biggest number,
+-- and keep doing that until we get 12 digits.
+-- I am genuinely so surprised that this works.
+searchBank :: String -> String
+searchBank = findMaximum
+  where finalSize = 12
+        findMaximum :: String -> String
+        findMaximum l
+          | length l <= finalSize = l
+          | otherwise = findMaximum (show . (maximum . map read . removals :: String -> Integer) $ l)
+          
 
 solve :: [String] -> Int
-solve = sum . map (maximum . map read . combinations)
+solve = sum . map (read . searchBank)
 
 main :: IO ()
 main = do
-  contents <- lines <$> readFile "inputs/day3-test.txt"
+  contents <- lines <$> readFile "inputs/day3.txt"
   print (solve contents)
 
