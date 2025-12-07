601f1cda8236e0c2f1fdd71fe06546c3dfcdb1fa
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2025-12-07 17:48:58 +1030

    Fix benign mistake in day 7 part 1

--- solutions/day7-1.hs
+++ solutions/day7-1.hs
@@ -32,18 +32,18 @@ mapToInput = transpose . map (map (tileToChar . snd)) . groupBy (\((x1, _),_) ((
           | otherwise = error "Invalid tile"
 
 simulate :: TileMap -> TileMap
-simulate m = foldr moveDown m beams
-  where beams = map fst . filter ((==TachyonBeam) . snd) . Map.toList $ m
-        moveDown (x,y)
-          | isNothing maybeBelow = id
-          | below == Empty = Map.insert (x,y+1) TachyonBeam
+simulate initialMap = foldr moveDown initialMap beams
+  where beams = map fst . filter ((==TachyonBeam) . snd) . Map.toList $ initialMap
+        moveDown (x,y) m
+          | isNothing maybeBelow = m
+          | below == Empty = Map.insert (x,y+1) TachyonBeam m
           | below == Splitter = useSplitter
           | below == UsedSplitter = useSplitter
-          | below == TachyonBeam = id
+          | below == TachyonBeam = m
           | otherwise = error ("Unexpected tile " ++ show below)
           where maybeBelow = Map.lookup (x,y+1) m
                 below = fromJust maybeBelow
-                useSplitter = Map.insert (x, y+1) UsedSplitter . Map.insert (x-1, y+1) TachyonBeam . Map.insert (x+1, y+1) TachyonBeam
+                useSplitter = Map.insert (x, y+1) UsedSplitter . Map.insert (x-1, y+1) TachyonBeam . Map.insert (x+1, y+1) TachyonBeam $ m
 
 solve :: [String] -> Int
 solve input = length . filter ((==UsedSplitter) . snd) . Map.toList . last . takeWhile (notElem TachyonBeam . finalRow) . iterate simulate $ tileMap
